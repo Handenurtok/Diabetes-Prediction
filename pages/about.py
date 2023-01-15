25c81e020b7bf7858bc1fdeb2c1c728e54fd5e07
Handenurtok <66180612+Handenurtok@users.noreply.github.com>
2023-01-15 15:15:41 +0300

    Create about.py

---
+++ pages/about.py
@@ -0,0 +1,14 @@
+"""This modules contains data about about page"""
+
+# Import necessary modules
+import streamlit as st
+
+def app():
+    """This function creates the about page"""
+    st.balloons()
+    st.title('Contact Us')
+    st.markdown('''### Name:
+    Hande Nur Tokpunar''')
+    st.markdown('''### Email:
+    handenur0707@gmail.com''')
+    st.markdown('''### GitHub: [HandeNurTok](https://github.com/Handenurtok/)''')
